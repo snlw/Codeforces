49c3f81887bc6ddfb287e5c80eec88684fba76d2
sean <sean.low@obviohealth.com>
2023-09-30 11:58:02 +0800

    405A

---
+++ A/405A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n;
+  cin >> n;
+
+  int a[n];
+
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  sort(a, a + n);
+
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
+
+  return 0;
+}
+
+
