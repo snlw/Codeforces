88d541dce4b844d43ed31faad565c612dbb6f927
sean <sean.low@obviohealth.com>
2023-11-10 22:42:58 +0800

    Update template

--- skeleton.cpp
+++ skeleton.cpp
@@ -4,10 +4,20 @@
 
 using namespace std;
 
+void solve() {
+
+}
+
 int main(){
 
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+
+  int t;
+  cin >> t;
+  while(t--) {
+    solve();
+  }
 
   return 0;
 }
